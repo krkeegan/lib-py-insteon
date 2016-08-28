79dfdc386716ce59909ab062c9fd484c21ee5396
Kevin Keegan <kevin@krkeegan.com>
2016-08-28 13:42:21 -0700

    Add Sphinx Documentation
    The start of user documentation.  Used for documenting the REST endpoints at
    the moment.

---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+.. Insteon REST Server documentation master file, created by
+   sphinx-quickstart on Sat Aug 27 16:59:35 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Insteon REST Server's documentation!
+===============================================
+
+The following endpoints are accessible.
+
+.. autobottle:: rest_server:Rest_Server(None)
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- insteon/rest_server.py
+++ insteon/rest_server.py
@@ -14,18 +14,57 @@ class Rest_Server(Bottle):
         self.route('/plms', callback=self.list_plms)
 
     def start(self):
-        threading.Thread(target=self.run, kwargs=dict(host='localhost', port=8080, debug=True)).start()
+        threading.Thread(target=self.run, kwargs=dict(
+            host='localhost', port=8080, debug=True)).start()
 
     def list_plms(self):
+        '''
+Returns an object containin all of the plms.
+
+**Example request**:
+
+.. sourcecode:: http
+
+  GET /plms HTTP/1.1
+  Host: example.com
+  Accept: application/json
+
+**Example response**:
+
+.. sourcecode:: http
+
+    HTTP/1.1 200 OK
+    Vary: Accept
+    Content-Type: application/json
+
+    {
+        "20F5F5": {
+            "dev_cat": 3,
+            "firmware": 155,
+            "port": "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A501LCKJ-if00-port0",
+            "port_active": false,
+            "sub_cat": 21
+        },
+        "3C4DB9": {
+            "dev_cat": 3,
+            "firmware": 158,
+            "port": "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A403KDV3-if00-port0",
+            "port_active": true,
+            "sub_cat": 21
+        }
+    }
+
+:statuscode 200: no error
+        '''
         plms = self._core.get_all_plms()
         ret = {}
         for plm in plms:
             ret[plm.dev_addr_str] = {
-                'dev_cat'       : plm.dev_cat,
-                'sub_cat'       : plm.sub_cat,
-                'firmware'      : plm.firmware,
-                'port'          : plm.port,
-                'port_active'   : plm.port_active
+                'dev_cat': plm.dev_cat,
+                'sub_cat': plm.sub_cat,
+                'firmware': plm.firmware,
+                'port': plm.port,
+                'port_active': plm.port_active
             }
         return self.jsonify(ret)
 
